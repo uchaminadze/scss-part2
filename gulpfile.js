8e9e4ebaa0c9159d5e2fdfe773db006fcdfa8e52
uchaminadze <73136591+uchaminadze@users.noreply.github.com>
2020-11-23 14:24:21 +0400

    add responsive

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+var cleancss = require('gulp-cleancss');
+
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleancss({keepBreaks: false}))
+    .pipe(gulp.dest('styles'));
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch('scss/**/*.scss', gulp.parallel('sass'));
+});
